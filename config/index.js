0c7b3fea48ca87da279d9c65082395c112f02280
astric <nittin1f4@gmail.com>
2023-09-30 13:36:56 +0530

    added reaper function to extract urls

---
+++ config/index.js
@@ -0,0 +1,16 @@
+const getDateString = _ => {
+    const dateStr = new Date().toLocaleString();
+    return dateStr.replaceAll(/(:|,|\s|\/)/g, '_');
+}
+
+const config = {
+    defaultUrl: 'https://github.com',    
+    intervalGap: 10000,
+    intervalRequestLimit: 6,
+    maxHostLimit: 6,
+    linkLimit: 20000,
+    targetDir: `./downloads/${ getDateString() }`,
+    targetDomain: false
+}
+
+export default config;
