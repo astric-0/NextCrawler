b3c811e443cdf9ffdac4079651de9a0bc5aa98b8
astric <nittin1f4@gmail.com>
2023-10-01 10:48:07 +0530

    added input form

--- components/SourceForm.jsx
+++ components/SourceForm.jsx
@@ -12,7 +12,7 @@ const SourceForm = _ => {
 
     return (
         <Form> 
-            <Row>
+            <Row className="justify-content-between">
                 <SourceField 
                     label="Url" 
                     md="4" 
@@ -35,7 +35,32 @@ const SourceForm = _ => {
                     value={ sourceState.intervalGap }
                     onChange={ setter('intervalGap') }
                 />
+                
+            </Row>
 
+            <Row className="justify-content-between">
+                <SourceField
+                    type="number"
+                    label="Max Host Limit"
+                    md="3"
+                    value={ sourceState.maxHostLimit }
+                    onChange={ setter('maxHostLimit') }
+                />
+
+                <SourceField
+                    type="number"
+                    label="Link Limit"
+                    md="3"
+                    value={ sourceState.linkLimit }
+                    onChange={ setter('linkLimit') }
+                />
+
+                <SourceField 
+                    label="Target Directory"
+                    md="3"
+                    value={ sourceState.targetDir }
+                    onChange={ setter('targetDir') }
+                />
             </Row>
         </Form>
     );
